0faa8de9e474819f77b8c97fc3c1814c5d626e6e
MuthuGowtham <30342311+MuthuGowtham@users.noreply.github.com>
2017-07-23 10:03:30 +0530

    Create conso.c

---
+++ conso.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char ch;
+printf("enter the number");
+scanf("%c",&ch);
+if(ch='a'||ch='e'||ch='i'||ch='o'||ch='u'||ch='A'||ch='E'||ch='I'||ch='O'||ch='U')
+{
+printf("the character is vowel");
+}
+else
+{
+printf("the character is consonant");
+}
+}
