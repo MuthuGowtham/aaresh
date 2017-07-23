f863cd2ef321ffeb45fad074a31f828dc929c2f8
MuthuGowtham <30342311+MuthuGowtham@users.noreply.github.com>
2017-07-23 11:58:12 +0530

    Create big.c

---
+++ big.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n1,n2,n3;
+printf("enter the number");
+scanf("%d%d%d",&n1,&n2,n3);
+if(n1>n2&&n1>n3)
+{
+printf("%d the bigger number",n1);
+}
+elseif(n2>n1&&n2>>n3)
+{
+printf("%d" the bigger number",n2);
+}
+elseif(n3>n1&&n3>n2)
+{
+printf("%d the bigger number",n3);
+}
+}
+
