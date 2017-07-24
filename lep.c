31ebdae087deb19cf56b81f3aebd9564b1a51e63
MuthuGowtham <30342311+MuthuGowtham@users.noreply.github.com>
2017-07-24 09:45:17 +0530

    Update lep.c

--- lep.c
+++ lep.c
@@ -5,4 +5,13 @@ void main()
 int year;
 printf("enter the year");
 scanf("%d",&year);
-if(year
+if(year%4==0 && year%100!=0 || year%400==0)
+{
+  printf("%d is leap year",year);
+}
+  else
+  {
+    printf("%d is not a leap yaer",year);
+  }
+}
+
